483f4ee3e824658b9b57a76c6adc7f71c7a3be55
kathir chidhambara Pandy <cskathir99@gmail.com>
2024-01-08 11:52:58 +0530

    Added custom user model and related test to use email as username

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,46 @@
+"""
+    Tests for models
+"""
+
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.core.management import call_command
+
+
+# Calling wait for db before executing test
+call_command('wait_for_db')
+
+class ModelTests(TestCase):
+
+    def test_user_created_by_email_successful(self):
+        """
+            Test to check whether user can be created via mail id sucessfully
+        """
+
+        email = 'test@gmail.com'
+        password = 'testpassword123'
+        
+        user = get_user_model().objects.create_user(
+            email = email,
+            password = password
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_user_email_normalized(self):
+        """
+            Test to check whether user emails are normalized after creation
+        """
+
+        sample_emails = [
+            ['test1@EXAMPLE.com', 'test1@example.com'],
+            ['Test2@Example.com', 'Test2@example.com'],
+            ['TEST3@EXAMPLE.com', 'TEST3@example.com'],
+            ['test4@example.COM', 'test4@example.com'],
+        ]
+
+        for email, expected_email in sample_emails:
+            user = get_user_model().objects.create_user(email = email, 
+                                                        password = 'test123')
+            self.assertEqual(user.email, expected_email)
